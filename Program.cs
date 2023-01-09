85479171b104b6d70f83f20a6076afbeae06ee9a
bloody <novickiy.n.l@gmail.com>
2023-01-09 20:21:30 +0300

    nothing_bad

--- Program.cs
+++ Program.cs
@@ -11,35 +11,53 @@ void Print (string[] a)
   }  
 }
 
-void PrintChange (string[] a)
+void Array (string[] a,string[] f)
 {
-  int i=0;
-  int box =-1;
-  int size =new Random().Next(0,3);
-  int box1=-1;
-
-  while (i<=size)
-  {
-    int randIndex=new Random().Next(0,a.Length);
-    if (randIndex!=box)
+  int i = 0;
+  a=new string[5];
+  f=new string[5];
+  int chars=3;
+    for (i = 0; i <= a.Length-1; i++)
     {
-      if (randIndex!=box1)
+      if (a[i].Length<=chars)
       {
-        box1=box;
-        System.Console.Write($"[{a[randIndex]+"]"+" "}");
-        i++;
-        box=randIndex;
-      } 
+        a[i]=f[i];
+      }
     }
-  }
+  //  return f[i];
 }
+
+
+
+// void PrintChange (string[] a)
+// {
+//   int i=0;
+//   int box =-1;
+//   int size =new Random().Next(0,3);
+//   int box1=-1;
+
+//   while (i<=size)
+//   {
+//     int randIndex=new Random().Next(0,a.Length);
+//     if (randIndex!=box)
+//     {
+//       if (randIndex!=box1)
+//       {
+//         box1=box;
+//         System.Console.Write($"[{a[randIndex]+"]"+" "}");
+//         i++;
+//         box=randIndex;
+//       } 
+//     }
+//   }
+// }
 
 int N=5;
 //string[] t=new string[N];
 string[] t;//не присвоенное (null)
 t=new string[N];//создаем массив, то есть в t помещаем адрес массива
-string a ="asdfg";
-string b ="qwert";
+string a ="as";
+string b ="qwe";
 string c ="zxcvb";
 string d ="';lk'";
 string e ="mnbvcv";
@@ -48,8 +66,10 @@ t[1]=b;
 t[2]=c;
 t[3]=d;
 t[4]=e;
-
+string[] q=new string[2];
 Print(t);
+Array(t,q);
 System.Console.WriteLine();
-PrintChange(t);
+Print(q);
+
 
